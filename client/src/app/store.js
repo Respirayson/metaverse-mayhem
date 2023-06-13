55d8a0262a8b150d0a4f94f653930deb0acebdce
rayson <raysonyeap4c@gmail.com>
2023-06-13 22:58:04 +0800

    basic workings of socket functionality

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,15 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
-import rootReducer from '../reducers';
-import { newRandomCard } from '../utils/cards';
-import { v4 as uuidv4 } from 'uuid';
+import { configureStore } from "@reduxjs/toolkit";
+import rootReducer from "../reducers";
+import { newRandomCard } from "../utils/cards";
+import { v4 as uuidv4 } from "uuid";
+import { io } from "socket.io-client";
 
-const initialHand = { cards:
-  Array(4).fill(0).map(() => Object.assign({}, newRandomCard(), { key: uuidv4() }))
-}
+const socket = io("http://localhost:8000");
+
+const logAction = (name) => {
+  return (data) => {
+    console.log(name, data);
+  };
+};
+
+socket.on("connect", logAction("connect"));
+socket.on("disconnect", logAction("disconnect"));
+socket.on("play_card", (payload) => {
+  console.log("Action has come through the socket ", payload.card);
+});
+
+const socketMiddleware = () => (next) => (action) => {
+  if (action.type === "PLAY_CARD") {
+    socket.emit("play_card", action);
+  }
+  return next(action);
+};
+
+const initialHand = {
+  cards: Array(4)
+    .fill(0)
+    .map(() => Object.assign({}, newRandomCard(), { key: uuidv4() })),
+};
 
 export default configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(socketMiddleware),
   preloadedState: {
     hand: initialHand,
-  }
-})
+  },
+});
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 import connectDB from "./mongodb/connect.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -11,27 +13,65 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/users', userRoutes);
-app.use('/api/v1/auth', authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/auth", authRoutes);
 
-app.get('/', async (req, res) => {
-    res.send("Hello from Metaverse Mayhem. This is to check whether the server is running.");
-})
+app.get("/", async (req, res) => {
+  res.send(
+    "Hello from Metaverse Mayhem. This is to check whether the server is running."
+  );
+});
+
+const httpServer = createServer();
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("disconnect", function () {
+    console.log("User Disconnected");
+  });
+
+  socket.on("play_card", (payload) => {
+    console.log("play_card", payload.payload.card);
+
+    socket.emit("play_card", payload.payload);
+  });
+});
+httpServer.listen(8000);
 
 const startServer = async () => {
+  try {
+    // MONGODB CONNECTION
+    connectDB(process.env.ATLAS_URL);
 
-    try {
-        connectDB(process.env.ATLAS_URL);
-        app.listen(5000, () => {
-            console.log("Server started on Port: 5000");
-        })
-    } catch (e) {
-        console.log(e);
-    }
+    // SERVER LISTENING
+    app.listen(5000, () => {
+      console.log("Server started on Port: 5000");
+    });
 
-    
-}
+    // SOCKET CONNECTION
+    io.on("connection", (socket) => {
+      console.log("Socket connected: " + socket.id);
+
+      socket.on("send_message", (data) => {
+        io.emit("message", data);
+      });
+
+      socket.on("disconnect", () => {
+        console.log("Socket disconnected: " + socket.id);
+      });
+    });
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 startServer();
