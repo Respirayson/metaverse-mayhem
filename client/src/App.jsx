bd189aa535d0639d253d7a5eb4b056fdecb27d4c
rayson <raysonyeap4c@gmail.com>
2023-06-26 00:32:24 +0800

    unit testing again

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -32,7 +32,7 @@ const App = () => {
     const checkAuthenticated = () => {
         const token = localStorage.getItem("token");
         if (token) {
-            fetch("http://127.0.0.1:5000/api/v1/auth/verify", {
+            fetch("https://metaverse-mayhem.vercel.app/api/v1/auth/verify", {
                 body: JSON.stringify({ token: token }),
                 headers: {
                     "Content-Type": "application/json",
